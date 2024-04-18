b97310389b8da8ae5d681de3ed61092fb6e727e5
hemendrasahu67 <57944144+hemendrasahu67@users.noreply.github.com>
2024-04-18 17:51:49 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Hemu148
+
+
+
+## Deploy To Heroku
+
+<a href="https://heroku.com/deploy?template=https://github.com/hemendrasahu67/naruto">
+     <img height="30px" src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku">
+  </a>
+
---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+API_ID = API_ID = 25298674
+
+API_HASH = os.environ.get("API_HASH", "1a7e01278b2bf0fe975fb8ce7c5ead8d")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6728485784:AAFHgTLL28oqvKWaO_ObX3a3oFIWruT_x54")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 6793357832))
+
+LOG = -1002120460486
+
